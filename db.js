57301ffac5b66072450ae36cd2b9e3573151c40e
arizworld <60315087+arizworld@users.noreply.github.com>
2022-05-27 22:48:12 +0530

    adding script for heroku db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // Connects to the data base 
-const mongoURI = "mongodb://localhost:27017/inotebooks";
+const mongoURI = "mongodb+srv://ArizWorld:Ariz.1234@inotebookapp.gievebh.mongodb.net/?retryWrites=true&w=majority";
 const connectToMongo  = ()=>{
     mongoose.connect(mongoURI,()=>{
         console.log('connected to mongoose');
